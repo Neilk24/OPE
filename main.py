be3660d18ea721de972eda3b28dc124d1148aa19
Neilk24 <neilk2050@gmail.com>
2024-08-26 17:45:11 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+age1=36
+age2=72
+age3=49
+age4=12
+age5=2
+age6=107
+
+sum=age1+age2+age3+age4+age5+age6
+print("The sum of all the ages is", sum)
+
+Average=sum//6
+
+print("The average of all the ages is~",Average)
